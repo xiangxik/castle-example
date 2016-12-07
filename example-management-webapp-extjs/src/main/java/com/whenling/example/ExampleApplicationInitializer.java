ee46c656f7fe2f55058f4a536582cc92446bedff
kongxiangxi <kongxiangxi@192.168.0.109>
2016-12-07 10:30:22 +0800

    menu

--- example-management-webapp-extjs/src/main/java/com/whenling/example/ExampleApplicationInitializer.java
+++ example-management-webapp-extjs/src/main/java/com/whenling/example/ExampleApplicationInitializer.java
@@ -39,11 +39,35 @@ public class ExampleApplicationInitializer extends ApplicationInitializer {
 		}
 		if (menuEntityService.count() == 0) {
 			int sortNo = 0;
-			menuEntityService.defaultCreate("用户管理", "user_managerment", "fa fa-users", "/user", null, sortNo++, null);
-			MenuEntity systemMenu = menuEntityService.defaultCreate("系统管理", "system_management", "fa fa-desktop", null, null, sortNo++, null);
-			menuEntityService.defaultCreate("系统设置", "system_setting", "fa fa-cogs", "/setting", null, sortNo++, systemMenu);
-			menuEntityService.defaultCreate("管理员", "system_admin", "fa fa-briefcase", "/admin", null, sortNo++, systemMenu);
-			menuEntityService.defaultCreate("管理组", "system_admin_group", "fa fa-gavel", "/group", null, sortNo++, systemMenu);
+
+			MenuEntity productManagement = menuEntityService.defaultCreate("产品管理", "product_management", "fa fa-cubes", null, null, sortNo++, null);
+			menuEntityService.defaultCreate("产品列表", "product", "fa fa-cube", "app.view.product.ProductList", null, sortNo++, productManagement);
+			menuEntityService.defaultCreate("产品分类", "product_category", "fa fa-filter", "app.view.productCategory.ProductCategoryList", null,
+					sortNo++, productManagement);
+			menuEntityService.defaultCreate("产品标签", "product_tag", "fa fa-bookmark", "app.view.productTag.ProductTagList", null, sortNo++,
+					productManagement);
+			menuEntityService.defaultCreate("品牌管理", "brand", "fa fa-th-large", "app.view.brand.BrandList", null, sortNo++, productManagement);
+
+			MenuEntity cms = menuEntityService.defaultCreate("内容管理", "cms_management", "fa fa-newspaper-o", null, null, sortNo++, null);
+			menuEntityService.defaultCreate("文章列表", "article", "fa fa-file", "app.view.article.ArticleList", null, sortNo++, cms);
+			menuEntityService.defaultCreate("文章分类", "article_category", "fa fa-filter", "app.view.articleCategory.ArticleCategoryList", null,
+					sortNo++, cms);
+			menuEntityService.defaultCreate("文章标签", "article_tag", "fa fa-bookmark", "app.view.articleTag.ArticleTagList", null, sortNo++, cms);
+
+			menuEntityService.defaultCreate("招聘管理", "job", "fa fa-archive", "app.view.job.JobList", null, sortNo++, cms);
+
+			MenuEntity userManagement = menuEntityService.defaultCreate("用户管理", "user_management", "fa fa-users", null, null, sortNo++, null);
+			menuEntityService.defaultCreate("用户列表", "member_list", "fa fa-user", "app.view.user.UserList", null, sortNo++, userManagement);
+			menuEntityService.defaultCreate("角色权限", "role_list", "fa fa-gavel", "app.view.role.RoleList", null, sortNo++, userManagement);
+
+			MenuEntity systemManagement = menuEntityService.defaultCreate("系统管理", "system_management", "fa fa-desktop", null, null, sortNo++, null);
+			menuEntityService.defaultCreate("参数设置", "parameter_setting", "fa fa-cogs", "app.view.setting.SettingForm", null, sortNo++,
+					systemManagement);
+			menuEntityService.defaultCreate("菜单管理", "menu_management", "fa fa-navicon", "app.view.menu.MenuList", null, sortNo++, systemManagement);
+			menuEntityService.defaultCreate("留言反馈", "feedback_management", "fa fa-crosshairs", "app.view.feedback.FeedbackList", null, sortNo++,
+					systemManagement);
+			menuEntityService.defaultCreate("邮件订阅", "subscribe_management", "fa fa-envelope-o", "app.view.subscribe.SubscribeList", null, sortNo++,
+					systemManagement);
 		}
 	}
 
